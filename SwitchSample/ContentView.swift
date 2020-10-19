62e29fc29e0128243c10638d8fe1a0d58d1f5885
picev0 <ecotech3690s@yahoo.co.jp>
2020-10-19 19:10:22 +0900

    Initial Commit

---
+++ SwitchSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwitchSample
+//
+//  Created by Shuhei Murata on 2020/10/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwitchSample/SwitchSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwitchSampleApp.swift
+//  SwitchSample
+//
+//  Created by Shuhei Murata on 2020/10/19.
+//
+
+import SwiftUI
+
+@main
+struct SwitchSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
